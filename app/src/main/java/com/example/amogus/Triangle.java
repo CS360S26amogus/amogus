52f867584a2359e35314762459d093a2465e1e5d
saim-bilal <27100208@lums.edu.pk>
2026-02-15 17:14:48 +0500

    triangle class added

---
+++ app/src/main/java/com/example/amogus/Triangle.java
@@ -0,0 +1,16 @@
+package com.example.amogus;
+
+public class Triangle extends Shape{
+    private float base;
+    private float height;
+
+    public Triangle(int x, int y, float base, float height) {
+
+        this.x = x;
+        this.y = y;
+        this.base = base;
+        this.height = height;
+
+    }
+
+}
